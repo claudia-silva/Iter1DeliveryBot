8c6988b14708e1526bdb10e8dcde80034e611a6c
Claudia Silva <1407431@brunel.ac.uk>
2018-03-02 23:29:40 +0000

    TrackParcelDialog class added to receive parcel update

---
+++ Iter1DeliveryBot/Dialogs/TrackParcelDialog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static Microsoft.Bot.Builder.Dialogs.PromptDialog;
+
+namespace Iter1DeliveryBot.Dialogs
+{
+    [Serializable]
+    public class TrackParcelDialog : IDialog<string>
+    {
+        private string trackingNo;
+
+        public TrackParcelDialog(string trackingNo)
+        {
+            this.trackingNo = trackingNo;
+        }
+        public Task StartAsync(IDialogContext context)
+        {
+            PromptDialog.Text(context, NextSteps, $@"Your parcel with Track No: {this.trackingNo} is currently with your local Hermes and will be delivered to you today by 4pm.");
+            return Task.CompletedTask;
+
+        }
+
+        public async Task NextSteps(IDialogContext context, IAwaitable<string> result)
+        {
+            await context.PostAsync("Is there anything else that DeliveryBot can help you with?");
+            PromptDialog.Choice(context, this.TrackParcelResumeAfter, new List<string>() { "Yes", "No" }, "Please select an option?");
+        }
+
+
+        public async Task TrackParcelResumeAfter(IDialogContext context, IAwaitable<string> result)
+        {
+            var optionSelected = await result;
+
+            switch (optionSelected)
+            {
+                case "Yes":
+                    await context.PostAsync("");
+                    context.Done(trackingNo);
+                    break;
+                case "No":
+                    await context.PostAsync("");
+                    PromptDialog.Text(context, TrackParcelResumeAfter, "Thank you for using DeliveryBot, Hope to speak with you again soon!");
+                    break;
+            }
+        }
+    }
+}
