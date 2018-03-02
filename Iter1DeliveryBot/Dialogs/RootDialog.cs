0606e2de02230335b6c616c71b6fe9a1baeb7e8d
Claudia Silva <1407431@brunel.ac.uk>
2018-03-02 23:19:28 +0000

    RootDialog Conversation Starter

--- Iter1DeliveryBot/Dialogs/RootDialog.cs
+++ Iter1DeliveryBot/Dialogs/RootDialog.cs
@@ -2,6 +2,10 @@
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static Microsoft.Bot.Builder.Dialogs.PromptDialog;
 
 namespace Iter1DeliveryBot.Dialogs
 {
@@ -15,17 +19,43 @@ namespace Iter1DeliveryBot.Dialogs
             return Task.CompletedTask;
         }
 
-        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        public Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
-            var activity = await result as Activity;
+            PromptDialog.Text(context, NameEntered, @"Hi! What's your name?");
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
-
-            // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
-
-            context.Wait(MessageReceivedAsync);
+            return Task.CompletedTask;
         }
+
+        public async Task NameEntered(IDialogContext context, IAwaitable<string> result)
+        {
+            var nameEntered = await result;
+            await context.PostAsync($@"Hi {nameEntered}! I am DeliveryBot, I can help with the following:");
+            PromptDialog.Choice(context, this.InitialMenuSelection, new List<string>() { "Delivery", "Other" }, "Please select an option?");
+        }
+
+        public async Task InitialMenuSelection(IDialogContext context, IAwaitable<string> result)
+        {
+            var optionSelected = await result;
+
+            switch (optionSelected)
+            {
+                case "Delivery":
+                    await context.PostAsync($"You selected {optionSelected}");
+                    //context.Call(new DeliveryDialog(), this.NameEntered);
+
+                    break;
+                case "other":
+                    await context.PostAsync($"You selected {optionSelected}");
+                    break;
+            }
+        }
+
+        //public async Task DeliveryDialogResumeAfter(IDialogContext context, IAwaitable<string> result)
+        //{
+        //    var trackingNo = await result;
+        //    await context.PostAsync($@"You entered {trackingNo}, Are you sure this is your Tracking No?");
+
+        //}
+
     }
 }
